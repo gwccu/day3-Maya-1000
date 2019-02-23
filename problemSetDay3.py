ad463ab7176ed21ae33986a8900c8541edf3b01d
Maya-1000 <mayadoronrepa@gmail.com>
2019-02-23 17:00:17 +0000

    I was able to do 2/3 parts, I had to leave early

--- problemSetDay3.py
+++ problemSetDay3.py
@@ -1,1 +1,27 @@
-# File name: problemSetDay3.py
+integer = int(input("Tell me a number."))
+
+if integer % 2 == 0:
+    print("That is even.")
+else:
+    print("Why did you put in an odd number? I don't like them.")
+
+a = int(input("Tell me another number."))
+
+if a % 2 == 0:
+    print("That is even.")
+else:
+    print("Why did you put in an odd number? I don't like them.")
+
+b = int(input("Tell me a third number."))
+
+if b % 2 == 0:
+    print("That is even.")
+else:
+    print("Why did you put in an odd number? I don't like them.")
+
+if b % 2 == 0 and a % 2 == 0 and integer % 2 == 0:
+    print("all your numbers are even! wow!")
+else:
+    print("You put at least one odd number, which I didn't like. :(")
+
+print(" I didn't have time for part 3, as I needed to leave early. Sorry!")
